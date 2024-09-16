90182139787fa506aba5ca06402d633cf6676fa1
Yanlu Wang <yanlu@nordicneurolab.com>
2024-09-16 22:50:03 +0200

    improved documentation, removed redudant mask in 04_...

--- 03_firstlevel_glm/remodulateBlocks.sh
+++ 03_firstlevel_glm/remodulateBlocks.sh
@@ -6,9 +6,10 @@
 datadir=$1          # 1. <data directory>
 csv_file=$2         # 2. <corsi results csv>
 
-output_affix="model.b2-5.1D"
-output_affix1="model.b2-3.1D"
-output_affix2="model.b4-5.1D"
+# filenames for the models generated
+output_affix="model.b2-5.1D"        # this is used for the full model
+output_affix1="model.b2-3.1D"       # used for first split model
+output_affix2="model.b4-5.1D"       # used for the second split model
 
 filelist=`find $datadir -name '*.timing.1D'`
 
@@ -48,14 +49,18 @@ do
         fi
         c=$(($c+1))
     done
+
     # save one file
     single_line=`awk '{printf "%s ", $0}' ${i_path}/${subj_id}.${type_id}.${task_id}.model.mD`
     echo $single_line > ${i_path}/${subj_id}.${type_id}.${task_id}.${output_affix}
 
-    # split and save multiple files
+    # split and save multiple files, 
+    # uncomment these if you want to split the single model into two
+    # (currently using the second space as delimiter)
+
     # Split the input at the second space and save the two parts into two variables.
     line1=$(echo "$single_line" | sed 's/ /§/2; s/§.*//')    # Extract text before the second space.
-    line2=$(echo "$single_line" | sed 's/^[^ ]* [^ ]* //')
+    line2=$(echo "$single_line" | sed 's/^[^ ]* [^ ]* //')   # Extract text after the second space.
     # Save each line into a separate file.
     echo "$line1" > ${i_path}/${subj_id}.${type_id}.${task_id}.${output_affix1}
     echo "$line2" > ${i_path}/${subj_id}.${type_id}.${task_id}.${output_affix2}
